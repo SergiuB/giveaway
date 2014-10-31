7a67109ac0f877657db8bc744a5bd33fd0346ef5
Sergiu Buciuc <buciuc_sergiu@yahoo.com>
2014-10-31 15:00:03 +0200

    donation UI logic

---
+++ javascripts/controller.js
@@ -0,0 +1,62 @@
+angular.module('giveawayApp', ["firebase"])
+  .controller('ItemsController', ['$scope', "$firebase",
+    function($scope, $firebase) {
+      var ref = new Firebase("https://incandescent-inferno-6504.firebaseio.com/items");
+      $scope.sync = $firebase(ref);
+      $scope.sync.$asObject().$bindTo($scope, "items");
+    }
+  ])
+  .controller('DonationController', ['$scope',
+    function($scope) {
+
+      $scope.donateClick = function(itemId, sum) {
+        $scope.donationIntent = {
+          amount: sum,
+          name: "",
+          email: ""
+        };
+      };
+
+      $scope.donateAbort = function() {
+        $scope.donationIntent = null;
+      };
+
+      $scope.topDonation = function(itemId) {
+        if ($scope.items[itemId].donations) {
+          return _.max($scope.items[itemId].donations, function(donation) {
+            return donation.amount;
+          });
+        } else {
+          return null;
+        }
+
+      };
+
+      $scope.nextDonationAmount = function(itemId) {
+        var topDonation = $scope.topDonation(itemId);
+        return (topDonation) ? topDonation.amount + 10 : null;
+      };
+
+      $scope.commitDonation = function(itemId) {
+        $scope.sync.$ref().child(itemId + '/donations').push($scope.donationIntent, function(error) {
+          if (error) {
+            alert("Ne pare rau, pare ca s-a intamplat ceva neasteptat. Mai apasa o data acum sau revino mai tarziu." + error);
+          } else {
+            var userName = $scope.donationIntent.name;
+            $scope.$apply(function() {
+              $scope.donationIntent = null;
+            });
+            alert("Iti multumim " + userName + "!\nTe rugam sa urmaresti periodic pagina pana marti la ora 24 pentru a vedea daca cineva face o donatie mai mare pentru obiectul dorit.\nSorry, e o aplicatie facuta intr-o zi, nu vin update-uri pe email :)");
+          }
+        });
+
+        // if (!$scope.items[itemId].donations) {
+        //   $scope.items[itemId].donations = [];
+        //   $scope.items.$save();
+        // } else {
+        //   $scope.items[itemId].donations.$push($scope.donationIntent);
+        // }
+
+      };
+    }
+  ]);
